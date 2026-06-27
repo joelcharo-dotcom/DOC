e0b622e967fc251dc46858b24ce889877f1ca11c
joelcharo-dotcom <joelcharo@gmail.com>
2026-06-27 06:08:47 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import Formulas from './pages/Formulas';
 import FormulaForm from './pages/FormulaForm';
 import Notas from './pages/Notas';
 import NotaForm from './pages/NotaForm';
+import Contabilidad from './pages/Contabilidad';
 import Facturas from './pages/Facturas';
 import FacturaForm from './pages/FacturaForm';
 import FormulaExterna from './pages/FormulaExterna';
@@ -29,186 +30,36 @@ function App() {
           <Route path="/formula-externa" element={<FormulaExterna />} />
 
           {/* Rutas protegidas */}
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Inicio />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/" element={<ProtectedRoute><Layout><Inicio /></Layout></ProtectedRoute>} />
 
           {/* Clientes */}
-          <Route
-            path="/clientes"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Clientes />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/clientes/nuevo"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <ClienteForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/clientes/:id"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <ClienteDetalle />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/clientes/:id/editar"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <ClienteForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/clientes" element={<ProtectedRoute><Layout><Clientes /></Layout></ProtectedRoute>} />
+          <Route path="/clientes/nuevo" element={<ProtectedRoute><Layout><ClienteForm /></Layout></ProtectedRoute>} />
+          <Route path="/clientes/:id" element={<ProtectedRoute><Layout><ClienteDetalle /></Layout></ProtectedRoute>} />
+          <Route path="/clientes/:id/editar" element={<ProtectedRoute><Layout><ClienteForm /></Layout></ProtectedRoute>} />
 
           {/* Historias */}
-          <Route
-            path="/historias"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Historias />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/historias/nueva"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <HistoriaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/historias/:id/editar"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <HistoriaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/historias" element={<ProtectedRoute><Layout><Historias /></Layout></ProtectedRoute>} />
+          <Route path="/historias/nueva" element={<ProtectedRoute><Layout><HistoriaForm /></Layout></ProtectedRoute>} />
+          <Route path="/historias/:id/editar" element={<ProtectedRoute><Layout><HistoriaForm /></Layout></ProtectedRoute>} />
 
           {/* Fórmulas */}
-          <Route
-            path="/formulas"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Formulas />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/formulas/nueva"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <FormulaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/formulas/:id/editar"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <FormulaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/formulas" element={<ProtectedRoute><Layout><Formulas /></Layout></ProtectedRoute>} />
+          <Route path="/formulas/nueva" element={<ProtectedRoute><Layout><FormulaForm /></Layout></ProtectedRoute>} />
+          <Route path="/formulas/:id/editar" element={<ProtectedRoute><Layout><FormulaForm /></Layout></ProtectedRoute>} />
 
           {/* Notas */}
-          <Route
-            path="/notas"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Notas />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/notas/nueva"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <NotaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/notas/:id/editar"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <NotaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/notas" element={<ProtectedRoute><Layout><Notas /></Layout></ProtectedRoute>} />
+          <Route path="/notas/nueva" element={<ProtectedRoute><Layout><NotaForm /></Layout></ProtectedRoute>} />
+          <Route path="/notas/:id/editar" element={<ProtectedRoute><Layout><NotaForm /></Layout></ProtectedRoute>} />
+
+          {/* Contabilidad */}
+          <Route path="/contabilidad" element={<ProtectedRoute><Layout><Contabilidad /></Layout></ProtectedRoute>} />
 
           {/* Facturas */}
-          <Route
-            path="/facturas"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Facturas />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/facturas/nueva"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <FacturaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/facturas/:id/editar"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <FacturaForm />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/facturas" element={<ProtectedRoute><Layout><Facturas /></Layout></ProtectedRoute>} />
+          <Route path="/facturas/nueva" element={<ProtectedRoute><Layout><FacturaForm /></Layout></ProtectedRoute>} />
+          <Route path="/facturas/:id/editar" element={<ProtectedRoute><Layout><FacturaForm /></Layout></ProtectedRoute>} />
 
           {/* Ruta por defecto */}
           <Route path="*" element={<Navigate to="/" replace />} />
